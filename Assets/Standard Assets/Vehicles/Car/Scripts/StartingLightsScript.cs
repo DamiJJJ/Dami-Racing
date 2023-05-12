ac77c6e0731e4c8d589080061479f544e32cc248
DamiJJJ <damipl1280@outlook.com>
2023-05-12 13:57:06 +0200

    Added start sounds, separate scenes

--- Assets/Standard Assets/Vehicles/Car/Scripts/StartingLightsScript.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/StartingLightsScript.cs
@@ -9,9 +9,13 @@ public class StartingLightsScript : MonoBehaviour
     public GameObject ALightOn;
     public GameObject GLightOff;
     public GameObject GLightOn;
+    public AudioSource Sound1;
+    public AudioSource Sound2;
+    public GameObject StartText;
 
     private void Start()
     {
+        StartText.SetActive(false);
         StartCoroutine(StartingLights());
     }
 
@@ -20,18 +24,24 @@ public class StartingLightsScript : MonoBehaviour
         yield return new WaitForSeconds(1);
         RLightOff.SetActive(false);
         RLightOn.SetActive(true);
+        Sound1.Play();
         yield return new WaitForSeconds(1);
         RLightOff.SetActive(true);
         RLightOn.SetActive(false);
+        Sound1.Play();
         ALightOff.SetActive(false);
         ALightOn.SetActive(true);
         yield return new WaitForSeconds(1);
         ALightOff.SetActive(true);
         ALightOn.SetActive(false);
+        Sound2.Play();
         GLightOff.SetActive(false);
         GLightOn.SetActive(true);
         yield return new WaitForSeconds(0.5f);
         SaveScript.RaceStart = true;
+        StartText.SetActive(true);
+        yield return new WaitForSeconds(2);
+        StartText.SetActive(false);
 
     }
 }
